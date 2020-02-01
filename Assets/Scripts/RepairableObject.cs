607cfdb934d812b44896e4241d2e58a28d37de0a
Kevin Kohler <kkohler@hitpointstudios.com>
2020-01-31 21:28:06 -0500

    Updated Main Scene.  Created script for RepairableObject

---
+++ Assets/Scripts/RepairableObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairableObject : MonoBehaviour
+{
+    List<GameObject> placeableObjects = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Rigidbody[] rs = gameObject.GetComponentsInChildren<Rigidbody>();
+        if (rs != null)
+        {
+            foreach (Rigidbody r in rs)
+            {
+                if (r != null && r.transform != null && r.transform.gameObject != null)
+                    placeableObjects.Add(r.transform.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
