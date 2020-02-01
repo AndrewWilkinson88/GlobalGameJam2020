c5deab436f78ebb127b1e859d71670fd7c1606b2
awilkinson <AndrewWilkinson88@gmail.com>
2020-02-01 16:33:16 -0500

    Cleaned up Object Controller a bit, shuffled scene elements

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class ObjectController : MonoBehaviour
 {
     public List<RepairableObject> repairableObjects = new List<RepairableObject>();
-
     private Vector3 offset;
     private Vector3 velocity = Vector3.zero;
 
     public GameObject selectedObject;
     public GameObject floor;
 
-    private bool isMoving = false;
+    private bool isObjectMoving = false;
     private float zCoord;
     private float smoothTime = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
+    {
+        if (isObjectMoving && selectedObject != null)
+        {
+            Vector3 newPos = GetMouseAsWorldPoint() + offset;
+            newPos[1] = Mathf.Clamp(newPos.y, floor.transform.position.y, 100.0f);
+            selectedObject.transform.position = Vector3.SmoothDamp(selectedObject.transform.position, newPos, ref velocity, smoothTime);
+        }
+    }
+
+    // Late Update is called once per frame, after all other Updates
+    private void LateUpdate()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -30,71 +35,53 @@ public class ObjectController : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, 100.0f, LayerMask.GetMask("unhit", "floor")))
             {
                 //Hit the Floor
-                if( hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("floor"))
+                if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer("floor"))
                 {
                     Camera.main.GetComponent<CameraController>().OnSelectCamera();
                 }
+                //Hit an Object
                 else
                 {
-                    Camera.main.GetComponent<CameraController>().OnDeselectCamera();
-
                     if (selectedObject != null && selectedObject != hitInfo.collider.gameObject)
                     {
-                        selectedObject.GetComponent<MeshRenderer>().material.color = Color.grey;
-                        selectedObject.GetComponent<Rigidbody>().useGravity = true;
-                        selectedObject.GetComponent<Rigidbody>().freezeRotation = false;
-                        selectedObject.GetComponent<Collider>().enabled = true;
-                        selectedObject = null;
+                        OnDeselectObject();
                     }
-                    
-                    if( selectedObject == null)
+
+                    if (selectedObject == null)
                     {
-                        selectedObject = hitInfo.collider.gameObject;
-                        //selectedObject.layer = LayerMask.NameToLayer("hit");
-                        selectedObject.GetComponent<MeshRenderer>().material.color = Color.red;
-                        selectedObject.GetComponent<Rigidbody>().useGravity = false;
-                        selectedObject.GetComponent<Rigidbody>().freezeRotation = true;
-                        selectedObject.GetComponent<Collider>().enabled = false;
+                        OnSelectObject(hitInfo.collider.gameObject);
                     }
 
                     zCoord = Camera.main.WorldToScreenPoint(selectedObject.transform.position).z;
                     offset = selectedObject.transform.position - GetMouseAsWorldPoint();
-                    isMoving = true;
+                    isObjectMoving = true;
                 }
+                Camera.main.GetComponent<CameraController>().OnDeselectCamera();
             }
-        }
-
-        if (isMoving && selectedObject != null)
-        {
-            Vector3 newPos = GetMouseAsWorldPoint() + offset;
-            newPos[1] = Mathf.Clamp(newPos.y, floor.transform.position.y, 100.0f);
-            selectedObject.transform.position = Vector3.SmoothDamp(selectedObject.transform.position, newPos, ref velocity, smoothTime);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             Camera.main.GetComponent<CameraController>().OnDeselectCamera();
-            isMoving = false;
+            isObjectMoving = false;
             if (selectedObject != null)
             {
-                foreach(RepairableObject r in repairableObjects)
+                foreach (RepairableObject r in repairableObjects)
                 {
                     bool tryPlace = r.TryPlace(selectedObject);
-                    if (!tryPlace)
+                    if (tryPlace)
                     {
-                        selectedObject.GetComponent<Collider>().enabled = true;
+                        OnLockObject();
                     }
                     else
                     {
-                        selectedObject.layer = LayerMask.NameToLayer("hit");
-                        selectedObject.GetComponent<MeshRenderer>().material.color = Color.grey;
-                        selectedObject = null;
+                        selectedObject.GetComponent<Collider>().enabled = true;
                     }
                 }
             }
         }
     }
-    
+
     private Vector3 GetMouseAsWorldPoint()
     {
         // Pixel coordinates of mouse (x,y)
@@ -105,5 +92,30 @@ public class ObjectController : MonoBehaviour
 
         // Convert it to world points
         return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    public void OnSelectObject(GameObject newObj)
+    {
+        selectedObject = newObj;
+        //selectedObject.layer = LayerMask.NameToLayer("hit");
+        selectedObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        selectedObject.GetComponent<Rigidbody>().useGravity = false;
+        selectedObject.GetComponent<Rigidbody>().freezeRotation = true;
+        selectedObject.GetComponent<Collider>().enabled = false;
+    }
+    public void OnDeselectObject()
+    {
+        selectedObject.GetComponent<MeshRenderer>().material.color = Color.grey;
+        selectedObject.GetComponent<Rigidbody>().useGravity = true;
+        selectedObject.GetComponent<Rigidbody>().freezeRotation = false;
+        selectedObject.GetComponent<Collider>().enabled = true;
+        selectedObject = null;
+    }
+
+    public void OnLockObject()
+    {
+        selectedObject.layer = LayerMask.NameToLayer("hit");
+        selectedObject.GetComponent<MeshRenderer>().material.color = Color.grey;
+        selectedObject = null;
     }
 }
